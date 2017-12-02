1ca6eb6e06f11ee5bdcbc340d4f2aaed4e113094
Kelvin Fernandes <sokelvinfernandes@gmail.com>
2017-12-02 20:18:48 -0300

    Criação do Banco de Dados SQLite3

--- src/database/dump_baby_spots.sql
+++ src/database/dump_baby_spots.sql
@@ -1,13 +1,11 @@
-CREATE SCHEMA IF NOT EXISTS babyspots;
-
-CREATE TABLE IF NOT EXISTS babyspots.servico
+CREATE TABLE servico
 (
     id_ser serial NOT NULL,
     nome text NOT NULL,
     CONSTRAINT pk_id_ser PRIMARY KEY (id_ser)
 );
 
-CREATE TABLE IF NOT EXISTS babyspots.usuario
+CREATE TABLE usuario
 (
     id_usu serial NOT NULL,
     nome_completo text NOT NULL,
@@ -17,7 +15,7 @@ CREATE TABLE IF NOT EXISTS babyspots.usuario
     CONSTRAINT pk_id_usu PRIMARY KEY (id_usu)
 );
 
-CREATE TABLE IF NOT EXISTS babyspots.estabelecimento
+CREATE TABLE estabelecimento
 (
     id_est serial NOT NULL,
     nome text NOT NULL,
@@ -32,12 +30,12 @@ CREATE TABLE IF NOT EXISTS babyspots.estabelecimento
     estado text NOT NULL,
     CONSTRAINT pk_id_est PRIMARY KEY(id_est),
     CONSTRAINT fk_id_usuario FOREIGN KEY (id_usuario)
-        REFERENCES babyspots.usuario(id_usu)
+        REFERENCES usuario(id_usu)
         ON UPDATE CASCADE
         ON DELETE SET NULL
 );
 
-CREATE TABLE IF NOT EXISTS babyspots.estabelecimento_foto
+CREATE TABLE estabelecimento_foto
 (
     id_esf serial NOT NULL,
     id_est integer NOT NULL,
@@ -45,12 +43,12 @@ CREATE TABLE IF NOT EXISTS babyspots.estabelecimento_foto
     data_cadastro date NOT NULL,
     CONSTRAINT pk_id_esf PRIMARY KEY(id_esf),
     CONSTRAINT fk_id_est FOREIGN KEY(id_est)
-        REFERENCES babyspots.estabelecimento(id_est) MATCH SIMPLE
+        REFERENCES estabelecimento(id_est) MATCH SIMPLE
         ON UPDATE CASCADE
         ON DELETE SET NULL
 );
 
-CREATE TABLE IF NOT EXISTS babyspots.avaliacao
+CREATE TABLE avaliacao
 (
     id_ava serial NOT NULL,
     id_est integer NOT NULL,
@@ -58,40 +56,40 @@ CREATE TABLE IF NOT EXISTS babyspots.avaliacao
     descricao text,
     CONSTRAINT pk_id_ava PRIMARY KEY(id_ava),
     CONSTRAINT id_est FOREIGN KEY(id_est)
-        REFERENCES babyspots.estabelecimento(id_est) MATCH SIMPLE
+        REFERENCES estabelecimento(id_est) MATCH SIMPLE
         ON UPDATE CASCADE
         ON DELETE SET NULL,
     CONSTRAINT id_usu FOREIGN KEY(id_usu)
-        REFERENCES babyspots.usuario(id_usu) MATCH SIMPLE
+        REFERENCES usuario(id_usu) MATCH SIMPLE
         ON UPDATE CASCADE
         ON DELETE SET NULL
 );
 
-CREATE TABLE IF NOT EXISTS babyspots.avaliacao_servico
+CREATE TABLE avaliacao_servico
 (
     id_ava integer NOT NULL,
     id_ser integer NOT NULL,
     pontuacao numeric NOT NULL,
     CONSTRAINT fk_id_ava FOREIGN KEY(id_ava)
-        REFERENCES babyspots.avaliacao(id_ava) MATCH SIMPLE
+        REFERENCES avaliacao(id_ava) MATCH SIMPLE
         ON UPDATE CASCADE
         ON DELETE SET NULL,
     CONSTRAINT fk_id_ser FOREIGN KEY(id_ser)
-        REFERENCES babyspots.aervico(id_ser) MATCH SIMPLE
+        REFERENCES aervico(id_ser) MATCH SIMPLE
         ON UPDATE CASCADE
         ON DELETE SET NULL
 );
 
-CREATE TABLE IF NOT EXISTS babyspots.estabelecimento_servico
+CREATE TABLE estabelecimento_servico
 (
     id_est integer NOT NULL,
     id_ser integer NOT NULL,
     CONSTRAINT fk_id_est FOREIGN KEY(id_est)
-        REFERENCES babyspots.estabelecimento (id_est) MATCH SIMPLE
+        REFERENCES estabelecimento (id_est) MATCH SIMPLE
         ON UPDATE CASCADE
         ON DELETE SET NULL,
     CONSTRAINT fk_id_ser FOREIGN KEY(id_est)
-        REFERENCES babyspots.servico(id_ser) MATCH SIMPLE
+        REFERENCES servico(id_ser) MATCH SIMPLE
         ON UPDATE CASCADE
         ON DELETE SET NULL
 );
