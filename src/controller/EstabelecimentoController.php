0c7520ddb742219c3ff1743aa08f54eb96ef04e0
Kelvin Fernandes <sokelvinfernandes@gmail.com>
2017-12-16 17:40:37 -0300

    Excluindo resquicíos do merge

--- src/controller/EstabelecimentoController.php
+++ src/controller/EstabelecimentoController.php
@@ -54,12 +54,11 @@ class EstabelecimentoController {
 	}
 
 	public function home(){
-<<<<<<< HEAD
+
 		$result = $this->factory_estabelecimento->listar();
 		$servicos = $this->factory_servico->listar();
-=======
+
 		$result = $this->factory_estabelecimento->listarTopEstabelecimentos();
->>>>>>> 3573b4f00fafa89b663172d78a2c5e40d30affed
 
 		require 'view/home.php';
 	}
