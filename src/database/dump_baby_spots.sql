b52c010c02abf0a5ce2a74a2b34dcb10896966c6
Unknown <m.matheus.ale@gmail.com>
2017-12-17 14:47:08 -0200

    Atualização do dump da extrutura do bd; Atualização dos dados da extrutura do bd; Dump de dados; atualizado; Novos estabelecimentos; Dados da tabela serviços copiados para o dump de dados; Criação de um banco de dadso secundario com totod os dados secundario "new_data_base.db";

--- src/database/dump_baby_spots.sql
+++ src/database/dump_baby_spots.sql
@@ -1,7 +1,8 @@
 CREATE TABLE servico
 (
     id_ser INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    nome TEXT NOT NULL    
+    nome TEXT NOT NULL,
+    classe VARCHAR(15) NULL    
 );
 
 CREATE TABLE usuario
@@ -15,7 +16,7 @@ CREATE TABLE usuario
 
 CREATE TABLE estabelecimento
 (
-    id_est INTEGER NOT NULL,
+    id_est INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     nome TEXT NOT NULL,
     id_usuario INTEGER NOT NULL,
     data_cadastro DATE NOT NULL,
@@ -36,7 +37,7 @@ CREATE TABLE estabelecimento_foto
     id_est INTEGER NOT NULL,
     url_imagem TEXT NOT NULL,
     data_cadastro DATE NOT NULL,
-    CONSTRAINT fk_id_est FOREIGN KEY(id_est),
+    CONSTRAINT fk_id_est FOREIGN KEY(id_est)
         REFERENCES estabelecimento(id_est) 
 );
 
@@ -60,7 +61,7 @@ CREATE TABLE avaliacao_servico
     CONSTRAINT fk_id_ava FOREIGN KEY(id_ava)
         REFERENCES avaliacao(id_ava),
     CONSTRAINT fk_id_ser FOREIGN KEY(id_ser)
-        REFERENCES
+        REFERENCES usuario(id_usu)
 );
 
 CREATE TABLE estabelecimento_servico
